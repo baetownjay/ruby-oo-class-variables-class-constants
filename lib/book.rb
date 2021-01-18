8a8fb14abde0883144e7c9cdb11fbd0778feab48
Jason Herrera <baytownjason@yahoo.com>
2021-01-18 16:34:53 -0600

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,24 +1,20 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
-  @@all = []
-  @@GENRES = []
+  GENRES = []
 
   def initialize(title)
     @title = title
-    @@all << self
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
-  def self.genres(genre)
+  def genre=(genre)
     @genre = genre
-    @@GENRES << genre
-    @@GENRES
+    GENRES << genre
   end
-
 
 end
