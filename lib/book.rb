586026e880f054988a71a8f5b108ef17c18c8006
Jason Herrera <baytownjason@yahoo.com>
2021-01-18 08:11:19 -0600

    on self.genres class method

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,23 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  @@all = []
+  @@GENRES = []
+
   def initialize(title)
     @title = title
+    @@all << self
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+  def self.genres(genre)
+    @genre = genre
+    @@GENRES << genre
+    @@GENRES
+  end
+
 
 end
