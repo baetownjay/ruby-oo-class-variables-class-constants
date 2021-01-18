e1daf2c53fe57f97f267b2f964588585718ffa7f
Jason Herrera <baytownjason@yahoo.com>
2021-01-18 16:33:26 -0600

    commit tree 55dd60557a02c1fed74fb3e7ca089f54a128feca

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,22 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+    #if brand.not_in(BRANDS) binding.pry #BRANDS << brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  # def self.BRANDS
+  #   @@BRANDS.uniq
+  # end
 
 end
